297abba29d07c8d867c96bbb1487c1f7e0ce2ad1
Yohann HUEDRO <112839213+yohann-devweb3@users.noreply.github.com>
2023-08-01 10:03:13 +1100

    fix(ui): logical moved from views to controllers

--- app/modules/frontend/controllers/ModuleController.php
+++ app/modules/frontend/controllers/ModuleController.php
@@ -11,8 +11,23 @@ class ModuleController extends Controller
 {
     public function indexAction($param=null)
     {
-        $modules = Module::find();
-        $this->view->setVar('modules', $modules);
+        $modulesData = [];
+
+        // Charger les données du module pour chaque client
+        foreach (Module::find() as $module) {
+            $id = $module->getId();
+            $libelle = $module->getLibelle();
+
+            // Ajouter les données du client au tableau $clientData
+            $modulesData[] = [
+                'id' => $id,
+                'libelle' => $libelle,
+            ];
+        }
+
+        $this->view->setVar('modules', $modulesData);
+        // Charger la vue
+        $this->view->pick('module/index');
 
     }
 
